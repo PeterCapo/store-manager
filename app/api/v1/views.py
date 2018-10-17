e7fe21cad60e8c3c003a2024b766d761acb3f8f3
peter okoth <pokoth2009@gmail.com>
2018-10-17 16:07:00 +0300

    [Feature #161283480] GET specific sale endpoint

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,1 +1,15 @@
+from flask_restful import Resource
+from flask import Flask, jsonify, make_response, request
 
+sales = []
+products = []
+
+
+class SpecificProduct(Resource):
+    def get(self, id):
+        return make_response(jsonify(
+            {
+                'Status': "OK",
+                'Message': "Success",
+                'specific product': products[id]
+            }), 200)
